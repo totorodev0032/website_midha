ef2e68c26b279c7f0f257ebd32fe291d75761ac3
nagendra0032 <nagendrakumar0032@gmail.com>
2020-11-09 14:37:42 +0530

    courses data added and enviroment variables set

---
+++ backend/data/courses.js
@@ -0,0 +1,38 @@
+const courses = [
+    {
+        _id: 1,
+        name : 'Python Power Programming',
+        price: 3500,
+        batch_date: '02 Dec 2020',
+        level: 'Beginner',
+        hours: '50',
+        assignments: '20', 
+        coding_problems: '200+',
+        projects: 3
+    },
+    {
+        _id: 2,
+        name : 'Machine Learning Certification',
+        price: 5500,
+        batch_date: '22 Dec 2020',
+        level: 'Intermediate',
+        hours: '70',
+        assignments: '20', 
+        coding_problems: '200+',
+        projects: 3
+    },
+    {
+        _id: 3,
+        name : 'Full Stack Development',
+        price: 7500,
+        batch_date: '026 Dec 2020',
+        level: 'all levels',
+        hours: '100',
+        assignments: '25', 
+        coding_problems: '200+',
+        projects: '3'
+    },
+    
+]
+
+module.exports = courses;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
 const app = express();
+const courses = require('./data/courses');
+
+dotenv.config();
 
 app.get('/', (req,res) => {
-    res.send('API is runnig...');
+    res.send('hello')
 })
 
-app.listen(5000, ()=> console.log('Server is running at 5000'));
+//routes for all courses
+app.get('/courses', (req,res) => {
+    res.json(courses)
+})
+
+app.get('/courses/id', (req, res) => {
+    const course = courses.find(course => course._id === req.params.id)
+    res.json(course)
+} );
+
+PORT = process.env.PORT;
+
+app.listen(PORT, ()=> console.log(`Server is running at ${PORT} at ${process.env.NODE_ENV} mode `));
