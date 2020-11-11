982a002dd8ad8890c477f1508cf7200157e4e918
divyashasinha23 <divyashasinha23@gmail.com>
2020-11-11 18:48:06 +0530

    userModel updated

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema
+({
+    name: {
+        type: String,
+        required: true,
+    },
+    userName:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    isAdmin:{
+        type: Boolean,
+        required: true,
+    },
+});
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;
