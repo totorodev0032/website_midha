137ab6146a26d74c5c0aec2fb8f53f2cf15cc4a9
nagendra0032 <nagendrakumar0032@gmail.com>
2021-01-05 16:37:23 +0530

    profile section bud fixed

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,29 +1,28 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/UserModel');
 const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
 
 // json web token is verified
-const requireAuth = asyncHandler(async (req,res,next) => {
-    let token;
-    if(
-        req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
-    ) try{
-        token = req.headers.authorization.split(' ')[1].toString();
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decodedToken.id).select('-password');
-        next();
-    } 
-    catch(error){
-        console.error(error);
+const requireAuth = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  )
+    try {
+      token = req.headers.authorization.split(' ')[1].toString();
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decodedToken.id).select('-password');
+      next();
+    } catch (error) {
+      console.error(error);
       res.status(401);
       throw new Error('not authorized, no token');
     }
-    if(!token){
-        res.status(401);
-        throw new Error('not authorized, no token');
-    }
+  if (!token) {
+    res.status(401);
+    throw new Error('not authorized, no token');
+  }
 });
-
 
 module.exports = requireAuth;
--- backend/routes/authroute.js
+++ backend/routes/authroute.js
@@ -1,11 +1,11 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const authController = require('../controllers/authControllers');
 const requireAuth = require('../middleware/authMiddleware');
 
 const router = Router();
 
-router.post('/signup',authController.signup_post);
-router.post('/signin',authController.signin_post);
+router.post('/signup', authController.signup_post);
+router.post('/signin', authController.signin_post);
 router.route('/profile').get(requireAuth, authController.getUser);
 
 module.exports = router;
